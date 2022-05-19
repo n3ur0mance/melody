1ae7f511ab7c290d7b548df5d90ab39d5e9331ca
chernyshova.m <electrohurtz@gmail.com>
2022-05-19 11:08:32 +0300

    add modal window

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
 $(document).ready(function () {
   let currentFloor = 2;
-  $(".home-image path").on("mouseover", function() {
+  let counterUp = $(".counter-up");
+  let counterDown = $(".counter-down");
+  let floorPath = $(".home-image path");
+  let modal = $(".modal");
+  let modalCloseButton = $(".modal-close-button");
+  let viewFlatsButton = $(".view-flats");
+
+  floorPath.on("mouseover", function() {
+    floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
     $(".counter").text(currentFloor);
   });
+
+  floorPath.on("click", toggleModal);
+  modalCloseButton.on("click", toggleModal);
+  viewFlatsButton.on("click", toggleModal);
+
+  counterUp.on("click", function() {
+    if(currentFloor < 18) {
+      currentFloor++;
+      let usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false})
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor = ${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+
+  counterDown.on("click", function() {
+    if(currentFloor > 2) {
+      currentFloor--;
+      let usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false})
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor = ${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+
+  function toggleModal() {
+    modal.toggleClass("is-open");
+  }
 });
